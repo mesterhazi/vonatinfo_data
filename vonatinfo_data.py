3ba413bbe71c24bdd49d09ea4f726f6d70496d4f
mesterhazi <adam.mesterhazi@gmail.com>
2018-12-04 22:53:49 +0100

    Added some logging and error handling in _get_data()

--- vonatinfo_data.py
+++ vonatinfo_data.py
@@ -1,21 +1,39 @@
 import requests
 import threading
+import logging
 import json
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+handler = logging.FileHandler('vonat_data.log')
+
+logger.addHandler(handler)
 
 class VonatDataGetter(threading.Thread):
     def __init__(self, database, period_s=300, url='http://vonatinfo.mav-start.hu/map.aspx/getData'):
+        logger.info(f"Initializing VonatDataGetter : db:{database} period:{period_s} url:{url}")
         super().__init__()
         self.url = url
         self.period = period_s
         self.database = None  # placeholder for MySQL DB running on rpi
+        logger.info("Initialization done...")
 
     def _get_data(self, id=False, history=False):
-        r = requests.post('http://vonatinfo.mav-start.hu/map.aspx/getData',
-                          json={"a":"TRAINS","jo":{"history":history,"id":id}})
+        # TODO error handling
+        try:
+            r = requests.post(self.url,
+                              json={"a":"TRAINS","jo":{"history":history,"id":id}})
+            r.raise_for_status()
+        except requests.HTTPError:
+            logger.error(f"HTTP response error! - {r.status_code} - url:{self.url} history{history} id:{id}")
+        except:
+            logger.error(f"Unknown error occured when makig a POST request url:{self.url} history{history} id:{id}")
+            return None
+
         return r.text
 
     def _unpack_data(self, json_str):
+        # TODO error handling
         action = json_str['d']['action']
         param = json_str['d']['param']
         result = json_str['d']['result']
