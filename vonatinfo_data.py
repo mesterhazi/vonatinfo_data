f9df55f09325c2e96d3aa6f55a5fbfb82660e1f2
adam <adam.mesterhazi@gmail.com>
2018-12-13 22:18:08 +0100

    Added thread starter main function

--- vonatinfo_data.py
+++ vonatinfo_data.py
@@ -130,13 +130,12 @@ class VonatDataGetter(threading.Thread):
 
 if __name__ == '__main__':
     vonatinfo_data_getter = VonatDataGetter()
-    vonatinfo_data_getter.debug_run()
-    # vonatinfo_data_getter.start()
-    #
-    # try:
-    #     while True:
-    #         time.sleep(1)
-    # except KeyboardInterrupt:
-    #     vonatinfo_data_getter.stop()
-    # finally:
-    #     pass
+    # vonatinfo_data_getter.debug_run()
+    vonatinfo_data_getter.start()
+    try:
+        while True:
+            time.sleep(1)
+    except KeyboardInterrupt:
+        vonatinfo_data_getter.stop()
+    finally:
+        pass
