110162482897ffd17dd2fd7cec8563666dae82e8
mesterhazi <adam.mesterhazi@gmail.com>
2019-02-24 20:49:57 +0100

    Added config.py instead of hardcoded parameters

---
+++ config.py
@@ -0,0 +1,11 @@
+""" Database config for Upload_all_worker """
+upload_all_worker_conf = {
+    'database' : ('127.0.0.1', 'train_data'),
+    'user' : ('vonat_data_getter', 'user')
+}
+
+""" Database config for Final_delay_worker """
+final_delay_worker_conf = {
+    'database' : ('127.0.0.1', 'train_data'),
+    'user' : ('vonat_data_getter', 'user'),
+}
