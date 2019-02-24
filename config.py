370674e91832fed818db2206be80ac4fd62c490f
mesterhazi <adam.mesterhazi@gmail.com>
2019-02-24 21:01:19 +0100

    Added table line in config for upload_all_worker

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 """ Database config for Upload_all_worker """
 upload_all_worker_conf = {
     'database' : ('127.0.0.1', 'train_data'),
-    'user' : ('vonat_data_getter', 'user')
+    'user' : ('vonat_data_getter', 'user'),
+    'table' : 'trains'
 }
 
 """ Database config for Final_delay_worker """
